24e0a22750f49720e34e214e021e118fe066af73
Ubuntu <ubuntu@ip-172-31-1-248.us-west-2.compute.internal>
2013-07-21 20:08:38 +0000

    Fix spelling of buff

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  var buff = fs.readFileSync("index.html");
+  var buf = fs.readFileSync("index.html");
   response.send(buf.toString());
 });
 
